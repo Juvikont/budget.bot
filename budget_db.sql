18956d3f56ab3398f6865678f4bf3c35cd2cca40
Yuri Kanapelka <y.kanapelka@gmail.com>
2020-01-29 20:23:19 +0300

    Changed column names

--- budget_db.sql
+++ budget_db.sql
@@ -1,27 +1,25 @@
-create table budget
-(
-    code_name   varchar(255) primary key,
+create table budget(
+    codename varchar(255) primary key,
     daily_limit integer
 );
 
-create table categories
-(
-    code_name       varchar(255) primary key,
-    name            varchar(255),
-    is_main_expense boolean,
-    alias           text
-);
-create table expense
-(
-    id primary key,
-    sum                integer,
-    created            date,
-    category_code_name integer,
-    message            text,
-    FOREIGN KEY (category_code_name) REFERENCES categories (code_name)
+create table category(
+    codename varchar(255) primary key,
+    name varchar(255),
+    is_base_expense boolean,
+    aliases text
 );
 
-insert into categories(code_name, name, is_main_expense, alias)
+create table expense(
+    id integer primary key,
+    amount integer,
+    created date,
+    category_codename integer,
+    raw_text text,
+    FOREIGN KEY(category_codename) REFERENCES category(codename)
+);
+
+insert into category(codename, name, is_base_expense, aliases)
 values ('products', 'продукты', true, 'еда'),
        ('coffee & snacks', 'перекусы', true, 'кофе, чай, сендвич, кола, фанта, сникерс'),
        ('dinner', ' обед', true, 'столовая, ланч, бизнес-ланч, бизнес-ланч'),
@@ -32,7 +30,7 @@ values ('products', 'продукты', true, 'еда'),
        ('saas', 'подписки', false, 'yandex.music, подписки, hbo'),
        ('other', 'прочее',true,'');
 
-insert into budget (code_name, daily_limit)
+insert into budget (codename, daily_limit)
 values ('income', 1000);
 
 

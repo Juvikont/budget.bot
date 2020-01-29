8f3a2122e79fd740552a72aba46dbc7913ca7716
Yuri Kanapelka <y.kanapelka@gmail.com>
2020-01-29 20:25:13 +0300

    Methods update

--- db.py
+++ db.py
@@ -1,18 +1,18 @@
 import os
-from typing import Dict, Tuple, List
+from typing import Dict, List, Tuple
 
 import sqlite3
 
-conn = sqlite3.connect(os.path.join("db", "budget.db"))
+conn = sqlite3.connect(os.path.join("", "db"))
 cursor = conn.cursor()
 
 
 def insert(table: str, column_values: Dict):
     columns = ', '.join(column_values.keys())
     values = [tuple(column_values.values())]
-    placeholders = ', '.join('?' * len(column_values.keys()))
+    placeholders = ", ".join("?" * len(column_values.keys()))
     cursor.executemany(
-        f"INSERT {table} "
+        f"INSERT INTO {table} "
         f"({columns}) "
         f"VALUES ({placeholders})",
         values)
@@ -20,20 +20,21 @@ def insert(table: str, column_values: Dict):
 
 
 def fetchall(table: str, columns: List[str]) -> List[Tuple]:
-    columns_joined = ', '.join(columns)
-    cursor.execute(f'SELECT {columns_joined} FROM {table}')
+    columns_joined = ", ".join(columns)
+    cursor.execute(f"SELECT {columns_joined} FROM {table}")
     rows = cursor.fetchall()
     result = []
-    for i in rows:
-        dict = {}
+    for row in rows:
+        dict_row = {}
         for index, column in enumerate(columns):
-            dict[column] = i[index]
-        result.append(dict)
+            dict_row[column] = row[index]
+        result.append(dict_row)
+    return result
 
 
 def delete(table: str, row_id: int) -> None:
     row_id = int(row_id)
-    cursor.execute(f'DELETE FROM {table} WHERE id={row_id}')
+    cursor.execute(f"delete from {table} where id={row_id}")
     conn.commit()
 
 
@@ -43,14 +44,14 @@ def get_cursor():
 
 def _init_db():
     """ DB initialization"""
-    with open('budget_db.sql', 'r') as f:
-        init = f.read()
-    cursor.executescript(init)
+    with open("budget_db.sql", "r") as f:
+        sql = f.read()
+    cursor.executescript(sql)
     conn.commit()
 
 
-def check_db():
-    """Check if DB exists and if its not- create it"""
+def check_db_exists():
+    """Check if DB exists and if its not- initialize it"""
     cursor.execute("SELECT name FROM sqlite_master "
                    "WHERE type='table' AND name='expense'")
     table_exists = cursor.fetchall()
@@ -58,4 +59,5 @@ def check_db():
         return
     _init_db()
 
-check_db()
+
+check_db_exists()
